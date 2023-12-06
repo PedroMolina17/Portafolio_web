e4b83e3fc7ff58a1223d100372f9d2def2385365
Pedro Molina <pedro.molinanoa@gmail.com>
2023-12-05 20:20:21 -0500

    Finish 2

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -20,7 +20,6 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
           >
             <CodeBracketIcon className="h-10 w-10 absolute top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2  text-[#ADB7BE] cursor-pointer group-hover/link:text-white" />
           </Link>
-
           <Link
             href={previewUrl}
             className="h-14 w-14 border-2  relative rounded-full border-[#ADB7BE] hover:border-white group/link"
